c25ce6975c69fa54914a6e6c42c21dfe3cacb1e1
sohal <preetsohal8@gmail.com>
2022-05-31 02:44:57 -0400

    java demo file

---
+++ HelloWorld/src/main/java/mthree/Hello.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mthree;
+
+/**
+ *
+ * @author priitsohal
+ * email: preetsohal8@gmail.com
+ * date: 31 May 2022
+ * purpose: My first Hello world program in an IDE
+ */
+
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Priit!");
+    }
+    
+}
